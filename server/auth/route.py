21df7e4b8c2a95cb96c4330529205838825fcb06
Manthan007 <manthansharma7818@gmail.com>
2026-09-14 11:28:53 +0530

    client set up

--- server/auth/route.py
+++ server/auth/route.py
@@ -68,4 +68,4 @@ def signup_teacher(req: TeacherUser):
 @router.get("/login")
 def login(user=Depends(authenticate)):
     """Handles user login"""
-    return {"message": f"Welcome, {user}!"}
+    return {"message": f"Welcome, {user["username"]}!", "role": user["role"]}
